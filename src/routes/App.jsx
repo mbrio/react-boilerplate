d932d1e96b3849d8fe95fc3596e26308f9fe2e7c
Michael Diolosa <michael.diolosa@gmail.com>
2015-03-20 21:50:00 -0400

    Update to non-beta react-router

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,9 +1,7 @@
 import * as less from './App.less';
 import React from 'react';
 // TODO: Fix when react-router fully supports 0.13
-import Router from 'react-router/build/npm';
-
-const { RouteHandler, Link } = Router;
+import { RouteHandler, Link } from 'react-router';
 
 // A route handler that contains the entirety of the application.
 export default class App extends React.Component {
